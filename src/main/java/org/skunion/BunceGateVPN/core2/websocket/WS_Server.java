ddd84371804fa197fcd9c734ac5ecb8a87d44e21
Mealf <q100056@gmail.com>
2020-10-24 19:57:10 +0800

    Revert "No decryption is server onMessage (ping test is OK"
    This reverts commit 4e2ff2de0d252a14f43e68fdb68d69d75bd21cc1.

--- src/main/java/org/skunion/BunceGateVPN/core2/websocket/WS_Server.java
+++ src/main/java/org/skunion/BunceGateVPN/core2/websocket/WS_Server.java
@@ -124,9 +124,7 @@ public class WS_Server extends WebSocketServer{
 		WS_Package wsp = WSRecord.get(conn);
 		if(wsp!=null&&wsp.readyFlag) {
 			EventSender.sendLog("Recv : "+message.array().length);///////////////Debug
-			//wsp.sport.sendToVirtualDevice(wsp.ud.dh.decryption(message.array()));//解密 並送給switch
-			wsp.sport.sendToVirtualDevice(message.array());
-			
+			wsp.sport.sendToVirtualDevice(wsp.ud.dh.decryption(message.array()));//解密 並送給switch
 		}
 	}
 
